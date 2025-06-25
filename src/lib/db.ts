4b20e9ca5332cb5049c8e8a8933db928fbb34b57
Grser <isaacxino407@gmail.com>
2025-06-25 15:01:38 -0400

    lib ignore

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,13 @@
-// src/lib/db.ts
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-  host: 'db.clawn.cat',
+export const pool = mysql.createPool({
+  host: 'db.clawn.cat',       // Cambia según tu configuración
   user: 'conexiones',
   password: '1234',
   database: 'streamplus',
   waitForConnections: true,
   connectionLimit: 5,
+  queueLimit: 0,
 });
 
 export default pool;
